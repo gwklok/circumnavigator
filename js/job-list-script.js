22105ddb4df12753f6787c10e9fe9966e092c923
Dylan Dumesnil <dadumesnil@gmail.com>
2016-03-15 18:35:20 -0700

    job list is hooked up to scheduler API

--- js/job-list-script.js
+++ js/job-list-script.js
@@ -1,16 +1,49 @@
+var SCHEDULER_API_URL = "http://10.144.144.11:4567/api/";
+var REFRESH_TIME = 15000;
+
+/*******************************************************/
+var TableContents = "";
+
+function Initialize() {
+    TableContents = $("#pure-job-list").html();
+}
+
+function ClearJobs() {
+    $("#pure-job-list").html(TableContents);
+}
+
+function RenderJobs(jobs_data) {
+    var jobs_directive = {
+        ".pure-job-entry" : {
+            "job<-" : {
+                ".pure-job-id" : "job.job_id",
+                ".pure-job-name" : "job.job_name",
+                ".pure-job-status" : "job.current_state",
+                ".pure-job-status@class+" : StatusClass,
+                ".pure-job-created-at" : "job.created_at",
+                ".pure-job-best-energy" : "job.best_energy",
+                ".pure-job-progress" : ProgressFormatter,
+                "@data-url" : JobDetailsLink
+            }
+        }
+    };
+    $("#pure-job-list").render(jobs_data, jobs_directive);
+    $(".pure-job-entry").click(OnJobClick);
+}
+
 function StatusClass(a) {
-    switch(a.item.status) {
-        case "Initialized":
+    switch(a.item.current_state) {
+        case "INITIALIZED":
             return " text-info";
-        case "Running":
+        case "RUNNING":
             return " text-primary";
-        case "Paused":
+        case "PAUSED":
             return " text-muted";
-        case "Stopped":
+        case "STOPPED":
             return " text-warning";
-        case "Done":
+        case "DONE":
             return " text-success";
-        case "Failed":
+        case "FAILED":
             return " text-danger";
         default:
             return "";
@@ -18,7 +51,7 @@ function StatusClass(a) {
 }
 
 function ProgressFormatter(a) {
-    return a.item.progress + "%";
+    return (a.item.num_finished_tasks / a.item.num_total_tasks * 100) + "%";
 }
 
 function JobDetailsLink(a) {
@@ -29,73 +62,23 @@ function OnJobClick(event) {
     window.location = $(event.target).parent("tr").data("url");
 }
 
-var jobs_data = [
-    {
-        "job_id" : 1,
-        "job_name" : "Travelling Sailor",
-        "status" : "Initialized",
-        "created_at" : "12:55 12/12/2012",
-        "best_energy" : 21344.23,
-        "progress" : 55.2
-    },
-    {
-        "job_id" : 69,
-        "job_name" : "Travelling Sailor",
-        "status" : "Running",
-        "created_at" : "12:55 12/12/2012",
-        "best_energy" : 21344.23,
-        "progress" : 55.2
-    },
-    {
-        "job_id" : 103,
-        "job_name" : "Travelling Sailor",
-        "status" : "Paused",
-        "created_at" : "12:55 12/12/2012",
-        "best_energy" : 21344.23,
-        "progress" : 55.2
-    },
-    {
-        "job_id" : 456,
-        "job_name" : "Travelling Sailor",
-        "status" : "Stopped",
-        "created_at" : "12:55 12/12/2012",
-        "best_energy" : 21344.23,
-        "progress" : 55.2
-    },
-    {
-        "job_id" : 678,
-        "job_name" : "Travelling Sailor",
-        "status" : "Done",
-        "created_at" : "12:55 12/12/2012",
-        "best_energy" : 21344.23,
-        "progress" : 55.2
-    },
-    {
-        "job_id" : 2348,
-        "job_name" : "Travelling Sailor",
-        "status" : "Failed",
-        "created_at" : "12:55 12/12/2012",
-        "best_energy" : 21344.23,
-        "progress" : 55.2
-    }
-];
-
-var jobs_directive = {
-    ".pure-job-entry" : {
-        "job<-" : {
-            ".pure-job-id" : "job.job_id",
-            ".pure-job-name" : "job.job_name",
-            ".pure-job-status" : "job.status",
-            ".pure-job-status@class+" : StatusClass,
-            ".pure-job-created-at" : "job.created_at",
-            ".pure-job-best-energy" : "job.best_energy",
-            ".pure-job-progress" : ProgressFormatter,
-            "@data-url" : JobDetailsLink
-        }
-    }
-};
+function LoadJobs() {
+    $.ajax({
+        url: SCHEDULER_API_URL + "jobs",
+        type: "GET",
+        dataType: "json"
+    })
+    .done(function(data, textStatus, jqXHR) {
+        ClearJobs();
+        RenderJobs(data);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        alert("Error loading jobs: " + errorThrown)
+    });
+}
 
 $(document).ready(function() {
-    $(".pure-job-list").render(jobs_data, jobs_directive);
-    $(".pure-job-entry").click(OnJobClick);
+    Initialize();
+    LoadJobs();
+    setInterval(LoadJobs, REFRESH_TIME);
 });
